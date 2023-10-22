2ea5f466048392f68b25bc11d76c400235f7c476
Jerry Lee <oldratlee@gmail.com>
2023-10-23 00:16:16 +0800

    refactor: reorder functions; rename vars; use `walrus operator`

--- lswin.py
+++ lswin.py
@@ -44,32 +44,38 @@ class WindowInfo:
 
 
 def list_window_infos() -> Iterable[WindowInfo]:
-    return (
-        WindowInfo(
+    for w in Quartz.CGWindowListCopyWindowInfo(Quartz.kCGWindowListOptionAll, Quartz.kCGNullWindowID):
+        bounds = w.valueForKey_('kCGWindowBounds')
+        yield WindowInfo(
             pid=int(w.valueForKey_('kCGWindowOwnerPID')),
             win_id=int(w.valueForKey_('kCGWindowNumber')),
             rect=Rect(
-                int(w.valueForKey_('kCGWindowBounds').valueForKey_('X')),
-                int(w.valueForKey_('kCGWindowBounds').valueForKey_('Y')),
-                int(w.valueForKey_('kCGWindowBounds').valueForKey_('Width')),
-                int(w.valueForKey_('kCGWindowBounds').valueForKey_('Height')),
+                int(bounds.valueForKey_('X')), int(bounds.valueForKey_('Y')),
+                int(bounds.valueForKey_('Width')), int(bounds.valueForKey_('Height')),
             ),
             title=str(w.valueForKey_('kCGWindowOwnerName') or ''),
             subtitle=str(w.valueForKey_('kCGWindowName') or '')
         )
-        for w in Quartz.CGWindowListCopyWindowInfo(Quartz.kCGWindowListOptionAll, Quartz.kCGNullWindowID)
-    )
 
 
 def filter_window_infos(
     win_list: Iterable[WindowInfo],
-    exclude_0_area, keep_one_for_same_pid_rect
+    exclude_0_area: bool, keep_one_for_same_pid_rect: bool
 ) -> Iterable[WindowInfo]:
     if exclude_0_area:
         win_list = (w for w in win_list if w.rect.width > 0 and w.rect.height > 0)
     if keep_one_for_same_pid_rect:
         win_list = {(w.pid, w.rect.t()): w for w in win_list}.values()
     return win_list
+
+
+def sorted_window_infos(win_list: Iterable[WindowInfo], sort_keys: Iterable[str]) -> Iterable[WindowInfo]:
+    return sorted(
+        win_list, key=lambda w: tuple(
+            getattr(w, k) if hasattr(w, k) else getattr(w.rect, k)
+            for k in sort_keys
+        ) + (w.pid, w.win_id)
+    )
 
 
 def print_window_infos(win_list: Iterable[WindowInfo], no_headers: bool = False):
@@ -93,19 +99,10 @@ def print_window_infos(win_list: Iterable[WindowInfo], no_headers: bool = False)
         print(f"{'-' * max_pid_chars}  {'-' * max_wid_chars}  {'-' * max_rect_chars}"
               f"  {'-' * (80 - max_pid_chars - max_wid_chars - max_rect_chars - 6)}")
     # print items
-    for win in win_list:
-        title_info = f"[{win.title}]{'' if not win.subtitle else ' ' + win.subtitle}"
-        print(f"{win.pid: >{max_pid_chars}}  {win.win_id: >{max_wid_chars}}"
-              f"  {win.rect.s(): <{max_rect_chars}}  {title_info}")
-
-
-def sorted_window_infos(win_list: Iterable[WindowInfo], sort_keys: Iterable[str]) -> Iterable[WindowInfo]:
-    return sorted(
-        win_list, key=lambda w: tuple(
-            getattr(w, k) if hasattr(w, k) else getattr(w.rect, k)
-            for k in sort_keys
-        ) + (w.pid, w.win_id)
-    )
+    for w in win_list:
+        title_info = f"[{w.title}]{'' if not w.subtitle else ' ' + w.subtitle}"
+        print(f"{w.pid: >{max_pid_chars}}  {w.win_id: >{max_wid_chars}}"
+              f"  {w.rect.s(): <{max_rect_chars}}  {title_info}")
 
 
 def main():
@@ -135,16 +132,14 @@ def main():
         '-o', '--keep-one-for-same-pid-rect', dest='keep_one_for_same_pid_rect',
         default=False, action='store_true', help='keep only one window for same (pid, rect)')
     option_parser.add_option(
-        '-k', '--sort-key', dest='sort_keys', default=[], action='append', metavar='SORT_KEY',
+        '-k', '--sort-key', dest='sort_keys', metavar='SORT_KEY', default=[], action='append',
         help=f"sort key, can be {supported_sort_keys_str}; default sort key is (pid, win_id)")
     option_parser.add_option(
-        '-H', '--no-headers', dest='no_headers',
-        default=False, action='store_true', help='print no header line')
+        '-H', '--no-headers', dest='no_headers', default=False,
+        action='store_true', help='print no header line')
 
     options, _ = option_parser.parse_args()
-
-    illegal_sort_keys = tuple(k for k in options.sort_keys if k not in supported_sort_keys)
-    if illegal_sort_keys:
+    if illegal_sort_keys := tuple(k for k in options.sort_keys if k not in supported_sort_keys):
         print(f"Unsupported sort key: {', '.join(illegal_sort_keys)}!"
               f" supported sort keys: {supported_sort_keys_str}", file=sys.stderr)
         return 1
