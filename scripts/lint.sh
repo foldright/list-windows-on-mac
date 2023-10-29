8711358eb384a75bb46ed6e3462c7bc4547f8e1e
Jerry Lee <oldratlee@gmail.com>
2023-10-29 22:56:49 +0800

    build: add `isort` dev dependency, and add to `lint.sh`; update dev dependencies 📦

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -6,6 +6,8 @@ source scripts/_utils.sh
 
 log_and_run flake8 --count --statistics --show-source --max-complexity=10 --max-line-length=120 ./*.py tests
 
+log_and_run isort --line-length=120 --check-only --diff ./*.py tests
+
 # https://mypy.readthedocs.io/en/stable/running_mypy.html#missing-imports
 # https://mypy.readthedocs.io/en/stable/existing_code.html#ignoring-errors-from-certain-modules
 log_and_run mypy ./*.py tests
